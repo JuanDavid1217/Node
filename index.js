dfd84ea67d91f19b0915fe9a16e7419b8efd6cf8
juan <juand_david2001@hotmail.com>
2023-05-29 16:45:56 -0600

    Se realizan cambios

--- index.js
+++ index.js
@@ -19,10 +19,60 @@ app.options('*', cors());
 const port = 8080;
 
 app.get('/', (req, res, next) => {
-  res.send('kafka api - adsoft');
+  res.send('kafka api - CreazyDave');
 });
 
-const run = async (username) => {
+/*Para las reacciones*/
+const runreaction = async (username, publication_id, reaction)=>{
+  await producer.connect()
+  await producer.send({
+    topic: 'reactions',
+    messages:[{
+      'value': `{"name": "${username}",
+                 "publication:"${publication_id}",
+                 "reaction: "${reaction}}` 
+    }],
+  })
+  await producer.disconnect()
+}
+
+app.get('/reaction', (req, res, next) => {
+  const username = req.query.name;
+  const publication_id = req.query.publication_id;
+  const reaction = req.query.reaction;
+  res.send({ 'name' : username,
+             'publication' : publication_id,
+             'reaction': reaction} );
+  run(username).catch(e => console.error(`[example/producer] ${e.message}`, e))
+
+});
+
+/*Para los comentarios*/
+const runcomments = async (username, publication_id, comment)=>{
+  await producer.connect()
+  await producer.send({
+    topic: 'comments',
+    messages:[{
+      'value': `{"name": "${username}",
+                 "publication:"${publication_id}",
+                 "comment: "${comment}}` 
+    }],
+  })
+  await producer.disconnect()
+}
+
+app.get('/comment', (req, res, next) => {
+  const username = req.query.name;
+  const publication_id = req.query.publication_id;
+  const comment = req.query.comment;
+  res.send({ 'name' : username,
+             'publication' : publication_id,
+             'comment': comment} );
+  run(username).catch(e => console.error(`[example/producer] ${e.message}`, e))
+
+});
+
+/*const run = async (username) => {
 
     await producer.connect()
 //    await producer.send()
@@ -42,7 +92,7 @@ app.get('/like', (req, res, next) => {
   res.send({ 'name' : username } );
   run(username).catch(e => console.error(`[example/producer] ${e.message}`, e))
 
-});
+});*/
 
 app.listen(port,  () => 
 	console.log('listening on port ' + port
